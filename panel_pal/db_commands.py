83d0a3b815080f679228abedaee730b3da19097d
cytng <natalie.groves@postgrad.manchester.ac.uk>
2016-07-28 11:43:51 +0100

    added import_pref_transcripts method to db_commands.py

--- panel_pal/db_commands.py
+++ panel_pal/db_commands.py
@@ -284,12 +284,15 @@ def import_pref_transcripts(project, transcripts, panel_pal, refflat):
     for tx in tx_list:
         if tx != tx_list[0]:
             acc = tx.split('\t')[1].split('.')[0] #splits gene name from transcript and removes version from accession
-            print acc
+            try:
+                tx_id = query_db(rf, 'SELECT id FROM tx WHERE accession = ?', (acc,))[0].get('id')
+                pp.execute('INSERT INTO pref_tx (project_id, tx_id) VALUES (?,?)', (project_id, tx_id))
+            except IndexError:
+                print acc + ' not in refflat database'
 
-            tx_id = query_db(rf, 'SELECT id FROM tx WHERE accession = ?', (acc,))[0].get('id')
-            pp.execute('INSERT INTO pref_tx (project_id, tx_id) VALUES (?,?)', (project_id, tx_id))
+import_pref_transcripts('NGD', '/results/Analysis/MiSeq/MasterTranscripts/NGD_preferred_transcripts.txt', '/home/bioinfo/Natalie/wc/panel_pal/panel_pal/resources/panel_pal.db', '/home/bioinfo/Natalie/wc/panel_pal/panel_pal/resources/refflat.db')
 
-#import_pref_transcripts('NGD', '/results/Analysis/MiSeq/MasterTranscripts/NGD_preferred_transcripts.txt', '/home/bioinfo/Natalie/wc/panel_pal/panel_pal/resources/panel_pal.db', '/home/bioinfo/Natalie/wc/panel_pal/panel_pal/resources/refflat.db')
+import_pref_transcripts('CTD', '/results/Analysis/MiSeq/MasterTranscripts/CTD_preferred_transcripts.txt', '/home/bioinfo/Natalie/wc/panel_pal/panel_pal/resources/panel_pal.db', '/home/bioinfo/Natalie/wc/panel_pal/panel_pal/resources/refflat.db')
 
 def export_bed(panel, panel_pal, refflat):
     conn_panelpal = sqlite3.connect(panel_pal)
