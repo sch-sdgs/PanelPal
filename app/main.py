9648d90c8be665d7a3fbadd0c70536d34b151b75
cytng <natalie.groves@postgrad.manchester.ac.uk>
2017-07-24 10:10:41 +0100

    added JSGlue to main.py for docker

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@
 from app.panel_pal import app
 from flask_bootstrap import Bootstrap
 Bootstrap(app)
+from flask_jsglue import JSGlue
+jsglue = JSGlue(app)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
